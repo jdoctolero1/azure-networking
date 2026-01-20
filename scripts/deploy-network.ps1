ec0b9aa4b4dc60a3b2f7a550264fad983047d6f4
Justin Doctolero <jdoctolero1@gmail.com>
2026-01-19 20:42:04 -0500

    task: add output message to ./scripts/deploy-network.ps1. Format deployment name to be unique based on resource group and date/time

--- scripts/deploy-network.ps1
+++ scripts/deploy-network.ps1
@@ -3,7 +3,11 @@ param (
     [string]$DrRegion      = "eastus"
 )
 
+Write-Host "[INFO] Creating lab network resources in Primary Region: $PrimaryRegion"
 az deployment sub create `
+  --name deploy-network-$PrimaryRegion-$(Get-Date -Format 'yyyyMMddHHmmss') `
   --location $PrimaryRegion `
-  --template-file ../modules/rg/main.bicep `
-  --parameters ../environments/lab/main.bicepparam
+  --template-file ../infra/main.bicep `
+  --parameters ../environments/lab/primary.bicepparam
+
+  #TODO: Add DR region deployment once Bicep modules are updated
