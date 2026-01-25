09e3b19d7ba4cff81ce5399a3f067e44abe070b6
Justin Doctolero <jdoctolero1@gmail.com>
2026-01-24 20:09:06 -0500

    task: all deploy-network.ps1 to take -Environment as a parameter.

--- scripts/deploy-network.ps1
+++ scripts/deploy-network.ps1
@@ -1,24 +1,26 @@
 param (
+    [string]$Environment   = "lab",
     [string]$PrimaryRegion = "centralus",
     [string]$DrRegion      = "eastus"
 )
 
-Write-Host "[INFO] Creating lab network resources in Primary Region: $PrimaryRegion"
+Write-Host "[INFO] Creating $Environment network resources in Primary Region: $PrimaryRegion"
 az deployment sub create `
   --name deploy-network-$PrimaryRegion-$(Get-Date -Format 'yyyyMMddHHmm') `
   --location $PrimaryRegion `
   --template-file ./infra/main.bicep `
-  --parameters ./environments/lab/primary.bicepparam
+  --parameters ./environments/$Environment/primary.bicepparam
 
-Write-Host "[INFO] Creating lab network resources in Disaster Recovery Region: $DrRegion"
+Write-Host "[INFO] Creating $Environment network resources in Disaster Recovery Region: $DrRegion"
 az deployment sub create `
   --name deploy-network-$DrRegion-$(Get-Date -Format 'yyyyMMddHHmm') `
   --location $DrRegion `
   --template-file ./infra/main.bicep `
-  --parameters ./environments/lab/dr.bicepparam
+  --parameters ./environments/$Environment/dr.bicepparam
 
-  Write-Host "[INFO] Creating VNet Peering from Primary to DR region"
-  az deployment sub create `
+Write-Host "[INFO] Creating $Environment VNet Peering from Primary to DR region"
+az deployment sub create `
+  --name deploy-$Environment-network-peering-$(Get-Date -Format 'yyyyMMddHHmm') `
   --location $PrimaryRegion `
   --template-file ./infra/peering.bicep `
-  --parameters environment='lab' primaryLocation=$PrimaryRegion drLocation=$DrRegion
+  --parameters environment=$Environment primaryLocation=$PrimaryRegion drLocation=$DrRegion
