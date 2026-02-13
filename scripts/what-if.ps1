712667086e60425a91cb8a9ac97d433fa4fe6d9b
Justin Doctolero <jdoctolero1@gmail.com>
2026-02-13 08:45:07 -0500

    task: include what-if.ps1 script to preview changes made to networking if the deployment script is run.

---
+++ scripts/what-if.ps1
@@ -0,0 +1,26 @@
+param (
+    [string]$Environment   = "lab",
+    [string]$PrimaryRegion = "centralus",
+    [string]$DrRegion      = "eastus"
+)
+
+Write-Host "[INFO] What if $Environment network resources in Primary Region: $PrimaryRegion is deployed"
+az deployment sub what-if `
+  --name deploy-network-$PrimaryRegion-$(Get-Date -Format 'yyyyMMddHHmm') `
+  --location $PrimaryRegion `
+  --template-file ./infra/main.bicep `
+  --parameters ./environments/$Environment/primary.bicepparam
+
+Write-Host "[INFO] What if $Environment network resources in Disaster Recovery Region: $DrRegion is deployed"
+az deployment sub what-if `
+  --name deploy-network-$DrRegion-$(Get-Date -Format 'yyyyMMddHHmm') `
+  --location $DrRegion `
+  --template-file ./infra/main.bicep `
+  --parameters ./environments/$Environment/dr.bicepparam
+
+Write-Host "[INFO] What is $Environment VNet Peering from Primary to DR region is deployed"
+az deployment sub what-if `
+  --name deploy-$Environment-network-peering-$(Get-Date -Format 'yyyyMMddHHmm') `
+  --location $PrimaryRegion `
+  --template-file ./infra/peering.bicep `
+  --parameters environment=$Environment primaryLocation=$PrimaryRegion drLocation=$DrRegion
